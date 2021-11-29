9590df1ff6e856e35c918fc0d201d879956641ae
ajiqbrainx <94741631+ajiqbrainx@users.noreply.github.com>
2021-11-29 17:26:46 +0530

    Add files

---
+++ src/main/scala/Maxmium.scala
@@ -0,0 +1,12 @@
+object Maxmium extends App {
+  val a=List(1,4,2,77)
+  val min =a(0);
+  val max =a(0)
+ if (a==min){
+   println("It is minimum")
+   if (a==max){
+     println("It is maximum")}
+ }
+
+
+}
